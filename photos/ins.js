47cebcab0254c84601eedceecad7cbb7e1052487
wyf0912 <790710534@qq.com>
2018-03-13 18:17:50 +0800

    Site updated: 2018-03-13 18:17:47

--- photos/ins.js
+++ photos/ins.js
@@ -116,7 +116,7 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://blog-img-1253710694.coscd.myqcloud.com/min-img/' + data.link[i];
+          var minSrc = 'http://blog-img-1253710694.cos.ap-chengdu.myqcloud.com/min-img/' + data.link[i];
           var src = 'http://blog-img-1253710694.coscd.myqcloud.com/img/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
